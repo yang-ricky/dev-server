04eb036bcc99c840d8ace839fb1eb4a1760e4089
Ricky Yang <ricky.yang@ef.com>
2020-08-03 22:16:02 +0800

    support static resource in koa

--- koa-test/server.js
+++ koa-test/server.js
@@ -1,8 +1,13 @@
 var Koa = require('koa');
 var app = new Koa();
+var static = require('koa-static');
+
 const main = ctx => {
     ctx.response.body = 'Hello World';
 };
+
+// 让 koa 可以处理静态资源文件
+app.use(static('.'));
 
 app.use(main);
 
